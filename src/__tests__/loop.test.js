be486484488b62f49071787df38112587a031dd6
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2024-05-12 13:43:05 -0300

    Refactor loop module

--- src/__tests__/loop.test.js
+++ src/__tests__/loop.test.js
@@ -4,6 +4,7 @@ let f;
 
 beforeEach(() => {
     const loop = new Loop();
+
     f = loop.createFuture();
 });
 
@@ -13,7 +14,7 @@ test('sets result, does not set exception, and does not cancel', async () => {
     await expect(f).resolves.toBe(value);
     f.setException(new Error());
     await expect(f).resolves.toBe(value);
-    f.cancel('');
+    f.cancel('message');
     await expect(f).resolves.toBe(value);
 });
 
@@ -23,17 +24,17 @@ test('sets exception, does not set result, and does not cancel', async () => {
     await expect(f).rejects.toThrow(error);
     f.setResult({});
     await expect(f).rejects.toThrow(error);
-    f.cancel('');
+    f.cancel('message');
     await expect(f).rejects.toThrow(error);
 });
 
 test('cancels, does not set result, and does not set exception', async () => {
-    f.cancel('');
+    f.cancel('message');
     try {
         await f;
     } catch (error) {
         expect(error).toBeInstanceOf(CancelledError);
-        expect(typeof error.message).toBe('string');
+        expect(error.message).toBe('message');
         f.setResult({});
         await expect(f).rejects.toThrow(error);
         f.setException(new Error());
