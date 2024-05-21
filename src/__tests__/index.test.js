dae267c194d54a8454db251b658b1b4dd922f005
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2024-05-21 13:29:49 -0300

    Refactor index module

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -7,27 +7,38 @@ jest.mock('../client', () => {
     };
 });
 
-const URL = 's';
+const URL = 'ws://localhost:8889';
 
 let i;
 
-function createClient(readyState) {
-    return {
+function mockClient(readyState) {
+    const client = {
         connection: new Promise((resolve, reject) => {
             if (readyState === null) {
                 reject(new Error());
             } else {
-                resolve({ readyState });
+                resolve({
+                    readyState,
+                    close: jest.fn(),
+                });
             }
         }),
     };
-}
 
-function createConstructor(client) {
-    return (url) => {
+    Client.mockImplementation((url) => {
         expect(url).toBe(URL);
         return client;
-    };
+    });
+
+    return client;
+}
+
+function start() {
+    return i.start(URL);
+}
+
+function stop() {
+    return i.stop(URL);
 }
 
 beforeEach(() => {
@@ -38,30 +49,72 @@ afterEach(() => {
     jest.restoreAllMocks();
 });
 
-test('starts and does not restart', async () => {
-    const client = createClient(WebSocket.OPEN);
-    Client.mockImplementation(createConstructor(client));
-    await expect(i.start(URL)).resolves.toBe(client);
-    await expect(i.start(URL)).resolves.toBe(client);
+test('stops, starts, connects, starts, and stops', async () => {
+    await stop();
+    const client = mockClient(WebSocket.OPEN);
+    await expect(start()).resolves.toBe(client);
+    await expect(start()).resolves.toBe(client);
+    await stop();
+    const socket = await client.connection;
+    expect(socket.close).toHaveBeenCalledTimes(1);
 });
 
-test('starts and restarts', async () => {
-    const client0 = createClient(WebSocket.CLOSED);
-    Client.mockImplementation(createConstructor(client0));
-    await expect(i.start(URL)).resolves.toBe(client0);
-    const client1 = createClient(WebSocket.OPEN);
-    Client.mockImplementation(createConstructor(client1));
-    await expect(i.start(URL)).resolves.toBe(client1);
-});
-
-test('does not start and starts', async () => {
+test('starts, connects, disconnects, stops, starts, connects, and stops', async () => {
     const warn = jest.spyOn(console, 'warn');
-    const client0 = createClient(null);
-    Client.mockImplementation(createConstructor(client0));
-    await expect(i.start(URL)).resolves.toBe(client0);
-    const client1 = createClient(WebSocket.OPEN);
-    Client.mockImplementation(createConstructor(client1));
-    await expect(i.start(URL)).resolves.toBe(client1);
+    const client0 = mockClient(WebSocket.CLOSED);
+    await expect(start()).resolves.toBe(client0);
+    await stop();
+    const client1 = mockClient(WebSocket.OPEN);
+    await expect(start()).resolves.toBe(client1);
+    await stop();
+    const socket0 = await client0.connection;
+    expect(socket0.close).toHaveBeenCalledTimes(0);
+    const socket1 = await client1.connection;
+    expect(socket1.close).toHaveBeenCalledTimes(1);
+    expect(warn).toHaveBeenCalledTimes(1);
+    expect(warn).toHaveBeenCalledWith(expect.any(String));
+});
+
+test('starts, connects, disconnects, starts, reconnects, and stops', async () => {
+    const warn = jest.spyOn(console, 'warn');
+    const client0 = mockClient(WebSocket.CLOSED);
+    await expect(start()).resolves.toBe(client0);
+    const client1 = mockClient(WebSocket.OPEN);
+    await expect(start()).resolves.toBe(client1);
+    await stop();
+    const socket0 = await client0.connection;
+    expect(socket0.close).toHaveBeenCalledTimes(0);
+    const socket1 = await client1.connection;
+    expect(socket1.close).toHaveBeenCalledTimes(1);
+    expect(warn).toHaveBeenCalledTimes(1);
+    expect(warn).toHaveBeenCalledWith(expect.any(String));
+});
+
+test('starts, does not connect, stops, starts, connects, and stops', async () => {
+    const warn = jest.spyOn(console, 'warn');
+    const client0 = mockClient(null);
+    await expect(start()).resolves.toBe(client0);
+    await stop();
+    const client1 = mockClient(WebSocket.OPEN);
+    await expect(start()).resolves.toBe(client1);
+    await stop();
+    await expect(client0.connection).rejects.toThrow(Error);
+    const socket1 = await client1.connection;
+    expect(socket1.close).toHaveBeenCalledTimes(1);
+    expect(warn).toHaveBeenCalledTimes(1);
+    expect(warn).toHaveBeenCalledWith(expect.any(String));
+});
+
+test('starts, does not connect, starts, connects, and stops', async () => {
+    const warn = jest.spyOn(console, 'warn');
+    const client0 = mockClient(null);
+    await expect(start()).resolves.toBe(client0);
+    const client1 = mockClient(WebSocket.OPEN);
+    await expect(start()).resolves.toBe(client1);
+    await stop();
+    await expect(client0.connection).rejects.toThrow(Error);
+    const socket1 = await client1.connection;
+    expect(socket1.close).toHaveBeenCalledTimes(1);
     expect(warn).toHaveBeenCalledTimes(1);
     expect(warn).toHaveBeenCalledWith(expect.any(String));
 });
--- src/index.js
+++ src/index.js
@@ -7,10 +7,14 @@ export class Index {
 
     start(url) {
         const next = this.#start(url, this.clients[url]);
-
         this.clients[url] = next;
-
         return next;
+    }
+
+    stop(url) {
+        const prev = this.clients[url];
+        delete this.clients[url];
+        return this.#stop(prev);
     }
 
     async #start(url, prev) {
@@ -23,12 +27,34 @@ export class Index {
                 if (socket.readyState === WebSocket.OPEN) {
                     return client;
                 }
+
+                console.warn('Client has disconnected: trying to reconnect...');
             } catch (error) {
-                console.warn('Unexpected client stoppage');
+                console.warn('Client not connected: trying to connect...');
             }
         }
 
         return new Client(url);
+    }
+
+    async #stop(prev) {
+        if (prev) {
+            const client = await prev;
+
+            try {
+                const socket = await client.connection;
+
+                if (socket.readyState === WebSocket.OPEN) {
+                    socket.close();
+
+                    return;
+                }
+
+                console.warn('Client already disconnected');
+            } catch (error) {
+                console.warn('Client never connected');
+            }
+        }
     }
 }
 
