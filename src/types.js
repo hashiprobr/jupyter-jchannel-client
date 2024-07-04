248af1d4ad47b8b816f27eedffc9ce0e646a3ae0
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2024-07-04 15:16:26 -0300

    Add documentation to exceptions

--- src/types.js
+++ src/types.js
@@ -5,12 +5,27 @@ export class AbstractError extends Error {
     }
 }
 
+/**
+ * Indicates that an operation could not be performed because the performer is
+ * in an invalid state.
+ *
+ * For example, a message could not be sent because the client is not connected.
+ *
+ * @hideconstructor
+ */
 export class StateError extends AbstractError {
     constructor(message) {
         super(message, 'StateError');
     }
 }
 
+/**
+ * Indicates that an operation could not be performed in the kernel.
+ *
+ * Contains a simple message or the string representation of a kernel exception.
+ *
+ * @hideconstructor
+ */
 export class KernelError extends AbstractError {
     constructor(message) {
         super(message, 'KernelError');
