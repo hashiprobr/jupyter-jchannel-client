1fc0d630808955f465ef9d72891d6129a7429fc7
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2024-07-18 08:05:10 -0300

    Remove dev server headers

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -8,12 +8,6 @@ module.exports = {
             },
         ],
     },
-    devServer: {
-        headers: {
-            'Access-Control-Allow-Origin': 'http://localhost:8888',
-            'Access-Control-Allow-Methods': 'GET',
-        },
-    },
     output: {
         library: {
             name: 'jchannel',
